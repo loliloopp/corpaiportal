7f9e6da686a7d1ee3c7b5daad5f708963501d39c
eugene <mosgate@gmail.com>
2025-10-25 16:15:12 +0300

    удаление миграций

--- supabase/migrations/20251025143457_remove_unused_limit_columns.sql
+++
@@ -1,8 +0,0 @@
-﻿ALTER TABLE public.user_profiles
-DROP COLUMN IF EXISTS daily_request_limit_enabled,
-DROP COLUMN IF EXISTS monthly_request_limit,
-DROP COLUMN IF EXISTS monthly_request_limit_enabled,
-DROP COLUMN IF EXISTS daily_token_limit,
-DROP COLUMN IF EXISTS daily_token_limit_enabled,
-DROP COLUMN IF EXISTS monthly_token_limit,
-DROP COLUMN IF EXISTS monthly_token_limit_enabled;
